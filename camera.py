937368ce53b077e97f1d57595ee6395a6cdb84ff
Giogia <10487928@polimi.it>
2017-11-11 20:52:34 +0100

    New default settings and timer

--- camera.py
+++ camera.py
@@ -8,12 +8,15 @@ def take_picture(name):
   camera = picamera.PiCamera()
   
   camera.start_preview()
+  
+  time.sleep(1)
   camera.capture('%s.jpg' %name)
+  
   camera.stop_preview()
   
   
 
-def set_default_camera():
+def set_default_settings():
   
   camera = picamera.PiCamera()
   
@@ -23,7 +26,7 @@ def set_default_camera():
   camera.brightness = 50
   camera.saturation = 0
   camera.ISO = 0
-  camera.video_stabilization = False
+  camera.video_stabilization = True #originally false
   camera.exposure_compensation = 0
   camera.exposure_mode = 'auto'
   camera.meter_mode = 'average'
@@ -31,8 +34,8 @@ def set_default_camera():
   camera.image_effect = 'none'
   camera.color_effects = None
   camera.rotation = 0
-  camera.hflip = False
-  camera.vflip = False
+  camera.hflip = True #originally false
+  camera.vflip = True #originally false
   camera.crop = (0.0, 0.0, 1.0, 1.0)
   
 
