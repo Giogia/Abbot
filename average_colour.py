6e26658de5edeec4081c514f0cf08656ea97c35a
Giogia <10487928@polimi.it>
2017-11-13 23:37:50 +0100

    Update average_colour.py

--- average_colour.py
+++ average_colour.py
@@ -1,6 +1,9 @@
 from PIL import Image
+import matplotlib.pyplot as plt
 
-def average_image_colour(image):
+def average_image_colour(name):
+    image = Image.open(name)
+    image = image.resize((50,50))  # Small optimization
     width, height = image.size
     
     r_total = 0
@@ -19,12 +22,12 @@ def average_image_colour(image):
     return (r_total/count, g_total/count, b_total/count)
 
 #see r,g,b values
-image = Image.open('prova.jpg')
-image = image.resize((50,50))  # Small optimization
-average_colour = average_image_colour(image)
-print(average_colour)
+def print_average_image_colour(name)
+    average_colour = average_image_colour(image)
+    print(average_colour)
 
 #display colour
-import matplotlib.pyplot as plt
-plt.imshow([[(average_colour)]])
-plt.show
+def display_average_colour(name)
+    average_colour = average_image_colour(image)
+    plt.imshow([[(average_colour)]])
+    plt.show
