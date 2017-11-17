34d6efb0ec1c85934e013809a6934aff54ddf3cd
Giogia <10487928@polimi.it>
2017-11-17 18:16:44 +0100

    added web detection

--- modules/google_vision.py
+++ modules/google_vision.py
@@ -30,3 +30,50 @@ def find_labels(name):
     print('Labels:')
     for label in labels:
         print(label.description)
+        
+def detect_web(name):
+    """Detects web annotations given an image."""
+    client = vision.ImageAnnotatorClient()
+    
+    # The name of the image file to annotate
+    file_name = os.path.join(
+        os.path.dirname(__file__),name)
+
+    # [START migration_web_detection]
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    response = client.web_detection(image=image)
+    notes = response.web_detection
+
+    if notes.pages_with_matching_images:
+        print('\n{} Pages with matching images retrieved')
+
+        for page in notes.pages_with_matching_images:
+            print('Url   : {}'.format(page.url))
+
+    if notes.full_matching_images:
+        print ('\n{} Full Matches found: '.format(
+               len(notes.full_matching_images)))
+
+        for image in notes.full_matching_images:
+            print('Url  : {}'.format(image.url))
+
+    if notes.partial_matching_images:
+        print ('\n{} Partial Matches found: '.format(
+               len(notes.partial_matching_images)))
+
+        for image in notes.partial_matching_images:
+            print('Url  : {}'.format(image.url))
+
+    if notes.web_entities:
+        print ('\n{} Web entities found: '.format(len(notes.web_entities)))
+
+        for entity in notes.web_entities:
+            print('Score      : {}'.format(entity.score))
+            print('Description: {}'.format(entity.description))
+    # [END migration_web_detection]
+# [END def_detect_web]
+          
