5cf139a54d50391f6dd0ad713a943f9da1b83158
Giogia <10487928@polimi.it>
2017-11-30 01:07:30 +0100

    Update database_update.py

--- database_update.py
+++ database_update.py
@@ -6,40 +6,48 @@ import itertools
 
 
 internet.wait_for_internet_connection()
+print "connection is up, updating database"
 
 for photo in database.get_photos():
     
-    labels = google_vision.find_labels(photo)
-    images = google_vision.detect_web(photo)
     existing = False
-
-    for label in labels:
-      if database.check_label(label):
-            database.update_photo(photo,label)
-            existing = True
-            break
     
+    if not database.check_photo(photo):
+        labels = google_vision.find_labels(photo)
+        images = google_vision.detect_web(photo) 
+        print "new photo found"
+        
+    for word in words:
+        if database.check_word(label):
+            existing = True
+            print "existing word found"
+            break
+        
     if existing == True:
         for label in itertools.islice(labels, 2):
             
-            if not database.check_label(label):
-                database.insert_label(label)
+            if not database.check_word(label):
+                database.insert_word(label)
+                print "added new word: %s" % label
                 
-            if not database.check_photo_label(photo,label):
-                database.insert_photo(photo,label)
+            if not database.check_label(photo,label):
+                database.insert_label(photo,label)
+                print "inserted new label"
                 
-        database.update_label(label)
-  
-            
+        database.conn.commit()
+        
         for image in images:
             if not database.check_image(photo,image):
-                
                 database.insert_image(photo,image)
+                print "inserted new image"
+                
         database.conn.commit()
+        
     else:
         database.delete_photo(photo)
         database.conn.commit()
+        print "no existing word found, %s deleted from database" % photo
         
-print("database_update.py executed -- database is now up to date")
+print "database is now up to date"
 
 
