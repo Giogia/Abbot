5797d5c61b8380bf87d1c46d7af0325d0114cb46
Giogia <10487928@polimi.it>
2017-12-06 10:24:27 +0100

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ["camera","average_colour","google_vision","database","internet","accelerometer","scrape"]
+__all__ = ["camera","average_colour","google_vision","database","internet","accelerometer","scrape","neopixel"]
