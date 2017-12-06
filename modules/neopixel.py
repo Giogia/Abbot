bea76605e8bbd0dc1b6981bad3cb0dd1500c3627
Giogia <10487928@polimi.it>
2017-12-06 10:32:12 +0100

    Update neopixel.py

--- modules/neopixel.py
+++ modules/neopixel.py
@@ -1,8 +1,20 @@
 # Adafruit NeoPixel library port to the rpi_ws281x library.
 # Author: Tony DiCola (tony@tonydicola.com), Jeremy Garff (jer@jers.net)
 import atexit
+import time
 
 import _rpi_ws281x as ws
+
+# LED strip configuration:
+LED_COUNT      = 1      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0
+LED_STRIP      = ws.SK6812_STRIP_RGBW	
+#LED_STRIP      = ws.SK6812W_STRIP
 
 
 def Color(red, green, blue, white = 0):
